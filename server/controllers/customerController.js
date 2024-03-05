389a0c2ba9bac098d0fd49dceda82781b2d1092c
Joao-Natale <122991781+Joao-Natale@users.noreply.github.com>
2024-03-05 16:55:56 -0300

    Finalização do mvp

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -51,6 +51,20 @@ exports.homepage = async (req, res) => {
 
 } */
 
+ exports.about = async (req, res) => {
+    const locals = {
+            title: 'InteractiveApp About',
+            description: 'Sistema de gerenciamento de funcionários'
+        };
+
+        try {
+            res.render('about', locals);
+        } catch (error) {
+            console.log(error);
+        }
+
+}
+
 exports.addCustomer = async (req, res) => {
     const locals = {
             title: 'Adicionar Novo Funcionário',
@@ -93,6 +107,85 @@ exports.viewCustomer = async (req, res) => {
             locals,
             customer
         })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+exports.editCustomer = async (req, res) => {
+
+    try {
+        const customer = await Customer.findOne({ _id: req.params.id })
+
+        const locals = {
+            title: 'Editar dados do Funcionário',
+            description: 'Sistema de Gerenciamento de Funcionários'
+        };
+
+        res.render('customer/edit', {
+            locals,
+            customer
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+exports.editPostCustomer = async (req, res) => {
+    try {
+        await Customer.findByIdAndUpdate((req.params.id),{
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            tel: req.body.tel,
+            email: req.body.email,
+            details: req.body.details,
+            updatedAt: Date.now()
+        });
+        res.redirect(`/edit/${req.params.id}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+exports.deleteCustomer = async (req, res) => {
+    try {
+       await Customer.deleteOne({ _id: req.params.id });
+       res.redirect("/"); 
+    } catch (error) {
+       console.log(error); 
+    }
+}
+
+
+exports.searchCustomers = async (req, res) => {
+
+    const locals = {
+        title: 'Busca do registro de funcionário',
+        description: 'Sistema de Gerenciamento de Funcionários',
+    }
+
+    try {
+        let searchTerm= req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+        const customers = await Customer.find({
+            $or: [
+                { firstName: { $regex: new RegExp(searchNoSpecialChar, "i") }},
+                { lastName: { $regex: new RegExp(searchNoSpecialChar, "i") }},
+            ]
+        });
+
+        res.render("search", {
+            customers,
+            locals
+        })
+
     } catch (error) {
         console.log(error);
     }
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -6,10 +6,17 @@ const costumerController = require('../controllers/customerController');
     Costumer Routes
 */
 router.get('/', costumerController.homepage);
+router.get('/about', costumerController.about);
 
 router.get('/add', costumerController.addCustomer);
 router.post('/add', costumerController.postCustomer);
 
 router.get('/view/:id', costumerController.viewCustomer);
+
+router.get('/edit/:id', costumerController.editCustomer);
+router.put('/edit/:id', costumerController.editPostCustomer);
+router.delete('/edit/:id', costumerController.deleteCustomer);
+
+router.post('/search', costumerController.searchCustomers);
 
 module.exports = router;
