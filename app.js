99167f2ce229e94ba6b07b75e5b564a08a342bc7
Joao-Natale <122991781+Joao-Natale@users.noreply.github.com>
2024-03-13 08:43:45 -0300

    validações de login

--- app.js
+++ app.js
@@ -17,9 +17,39 @@ app.use(express.json())
 
 // Models
 const User = require('./server/models/User');
+const { restart } = require('nodemon');
 
 // Connect to Database
 connectDB();
+
+// Login User
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body
+
+    //validations
+    if (!email) {
+        return res.status(422).json({msg: 'O email é obrigatório!'})
+    }
+
+    if (!password) {
+        return res.status(422).json({msg: 'A senha é obrigatória!'})
+    }
+
+    // check if user exists
+    const user = await User.findOne({email: email})
+
+    if (!user) {
+        return res.status(404).json({msg: 'Usuário não encontrado!'})
+    }
+
+    // check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.status(422).json({msg: 'Senha inválida!'})
+    }
+
+})
 
 // Register User
 app.post('/auth/register', async(req, res) => {
@@ -39,7 +69,39 @@ app.post('/auth/register', async(req, res) => {
         return res.status(422).json({msg: 'A senha é obrigatória' })
     }
 
+    if(password !== confirmpassword) {
+        return res.status(422).json({msg: 'As senhas não conferem'})
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+        return res.status(422).json({ msg: 'Por favor, utilize outro e-mail!' })
+    }
+
+    // create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
+
+    try {
+        await user.save()
+
+        res.status(201).json({msg:'Usuário criado com sucesso!'})
+    } catch (error) {
+       console.log(error)
+       res.status(500).json({msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!'}) 
+    }
 })
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
